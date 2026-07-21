148fe37b5299e19e7e7009b02a3f4dc99aa28bc1
MohammedHamed12121 <mh9182731122@gmail.com>
2026-07-21 15:01:04 +0300

    Feat: Init Window

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+
+const int screenWidth = 900;
+const int screenHeight = screenWidth;
+const int pieces = 8;
+const int cellSize = screenHeight / pieces;
+
+Raylib.InitWindow(screenWidth, screenHeight, "Chess Game");
+
+Raylib.SetTargetFPS(60);               
+
+while (!Raylib.WindowShouldClose())    
+{
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.RayWhite);
+
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();        // Close window and OpenGL context
+
+
